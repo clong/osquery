45f9304617de0db8ec25635845acf67fc8087488
Chris Long <clong@cl0ng.com>
2017-10-02 16:14:30 -0700

    Fixing -args parameter in manage-osquery.ps1

--- tools/manage-osqueryd.ps1
+++ tools/manage-osqueryd.ps1
@@ -6,13 +6,13 @@
 #  of patent rights can be found in the PATENTS file in the same directory.
 
 param(
-  [string] $args = "",
+  [string] $startup_args = "",
   [switch] $install = $false,
   [switch] $uninstall = $false,
-  
+
   [switch] $start = $false,
   [switch] $stop = $false,
-  
+
   [switch] $help = $false,
   [switch] $debug = $false
 )
@@ -29,19 +29,19 @@ function Test-IsAdmin {
 
 function Do-Help {
   $programName = (Get-Item $PSCommandPath ).Name
-  
+
   Write-Host "Usage: $programName (-install|-uninstall|-start|-stop|-help)" -foregroundcolor Yellow
   Write-Host ""
   Write-Host "  Only one of the following options can be used. Using multiple will result in "
   Write-Host "  options being ignored."
   Write-Host "    -install          Install the osqueryd service"
-  Write-Host "    -args             Specifies additional arguments for the service (only used with -install)"
+  Write-Host "    -startup_args     Specifies additional arguments for the service (only used with -install)"
   Write-Host "    -uninstall        Uninstall the osqueryd service"
   Write-Host "    -start            Start the osqueryd service"
   Write-Host "    -stop             Stop the osqueryd service"
   Write-Host ""
   Write-Host "    -help              Shows this help screen"
-  
+
   Exit 1
 }
 
@@ -50,31 +50,31 @@ function Do-Service {
     Write-Host "'$kServiceBinaryPath' is not a valid file. Did you build the osquery daemon?" -foregroundcolor Red
     Exit -1
   }
-  
+
   $osquerydService = Get-WmiObject -Class Win32_Service -Filter "Name='$kServiceName'"
-  
+
   if ($install) {
     if ($osquerydService) {
       Write-Host "'$kServiceName' is already installed." -foregroundcolor Yellow
       Exit 1
     } else {
-      New-Service -BinaryPathName "$kServiceBinaryPath $args" -Name $kServiceName -DisplayName $kServiceName -StartupType Automatic
+      New-Service -BinaryPathName "$kServiceBinaryPath $startup_args" -Name $kServiceName -DisplayName $kServiceName -StartupType Automatic
       Write-Host "Installed '$kServiceName' system service." -foregroundcolor Cyan
       Exit 0
     }
   } elseif ($uninstall) {
     if ($osquerydService) {
       Stop-Service $kServiceName
-      
+
       Write-Host "Found '$kServiceName', stopping the system service..."
-      
+
       Start-Sleep -s 5
-      
+
       Write-Host "System service should be stopped."
-      
+
       $osquerydService.Delete()
       Write-Host "System service '$kServiceName' uninstalled." -foregroundcolor Cyan
-      
+
       Exit 0
     } else {
       Write-Host "'$kServiceName' is not an installed system service." -foregroundcolor Yellow
@@ -94,8 +94,8 @@ function Do-Service {
       Write-Host "'$kServiceName' system service is stopped." -foregroundcolor Cyan
     } else {
       Write-Host "'$kServiceName' is not an installed system service." -foregroundcolor Yellow
-      Exit 1      
-    }  
+      Exit 1
+    }
   } else {
     Write-Host "Invalid state: this should not exist!" -foregroundcolor Red
     Exit -1
@@ -107,17 +107,17 @@ function Main {
     Write-Host "Please run this script with Admin privileges!" -foregroundcolor Red
     Exit -1
   }
-  
+
   if ($help) {
     Do-Help
   } elseif ($debug) {
     $osquerydExists = Test-Path $kServiceBinaryPath
-    
+
     Write-Host "Service Information" -foregroundcolor Cyan
     Write-Host "  kServiceName       = '$kServiceName'" -foregroundcolor Cyan
     Write-Host "  kServiceBinaryPath = '$kServiceBinaryPath'" -foregroundcolor Cyan
     Write-Host "             +exists = $osquerydExists" -foregroundcolor Cyan
-    
+
     Exit 0
   } elseif (($install.ToBool() + $uninstall.ToBool() + $start.ToBool() + $stop.ToBool()) -Eq 1) {
     # The above is a dirty method of determining if only one of the following booleans are true.
